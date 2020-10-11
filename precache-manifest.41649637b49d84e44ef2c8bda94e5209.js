bd441d136a8a5629a9940973671e2d6913e481c9
Apoorv <apoorv.ydv17@gmail.com>
2020-10-11 20:35:32 +0530

    Updates

---
+++ precache-manifest.41649637b49d84e44ef2c8bda94e5209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4464894ae47360bf9d03b70570b2e366",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3e775dafc7d204464d62",
+    "url": "/monster-rolodex/static/css/main.c28a20fd.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e775dafc7d204464d62",
+    "url": "/monster-rolodex/static/js/main.d77390d1.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
